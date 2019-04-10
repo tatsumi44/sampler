f07f18ed3139e94a3f75f1ec7715e79348ac602e
k-tatsumi <tatsuken4290@gmail.com>
2019-04-11 00:11:04 +0900

    first_commit

---
+++ Podfile
@@ -0,0 +1,11 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '9.0'
+
+target 'TinderUI_Sampler' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+  pod 'Nuke', '~> 7.0'
+  pod ‘Poi’
+  # Pods for TinderUI_Sampler
+
+end
---
+++ TinderUI_Sampler/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TinderUI_Sampler
+//
+//  Created by k-tatsumi on 2019/04/10.
+//  Copyright © 2019 k-tatsumi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
